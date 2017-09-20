b0336af188aa5c23e5b90325b65bbe7d45943c7d
James Grenning <james@grenning.net>
2017-09-20 16:51:59 +0200

    Added docker container.  Prototype, under construction

--- CppUTestCompileFlags.mk
+++ CppUTestCompileFlags.mk
@@ -1,12 +1,16 @@
 
-CPPUTEST_WARNINGFLAGS += -Wall 
-CPPUTEST_WARNINGFLAGS += -Werror
-CPPUTEST_WARNINGFLAGS += -Wswitch-default 
+# Figure out how to ask if gcc7 vs clang, gcc7 only right now
+ifeq "0" "1"
 CPPUTEST_WARNINGFLAGS += -Wno-unknown-warning-option
 CPPUTEST_WARNINGFLAGS += -Wno-covered-switch-default
 CPPUTEST_WARNINGFLAGS += -Wno-reserved-id-macro
 CPPUTEST_WARNINGFLAGS += -Wno-keyword-macro
 CPPUTEST_WARNINGFLAGS += -Wno-documentation
+endif
+
+CPPUTEST_WARNINGFLAGS += -Wall 
+CPPUTEST_WARNINGFLAGS += -Werror
+CPPUTEST_WARNINGFLAGS += -Wswitch-default
 CPPUTEST_WARNINGFLAGS += -Wno-format-nonliteral
 CPPUTEST_WARNINGFLAGS += -Wno-sign-conversion 
 CPPUTEST_WARNINGFLAGS += -Wno-pedantic 
